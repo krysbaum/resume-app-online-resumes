442b460bacfca97257d6cf87ebfef776d45d2035
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-04-16 19:27:34 -0700

    added incomplete show/index

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,44 @@
-<script></script>
+<!--
+  todo
+1. get backend 
+2. finish index convert to show 
+ -->
+
+<script>
+import axios from "axios";
+import ShowResume from "./ShowResume.vue";
+import IndexResume from "./IndexResume.vue";
+
+export default {
+  components: {
+    IndexResume,
+    ShowResume,
+  },
+  data: function () {
+    return {
+      students: [],
+    };
+  },
+  created: function () {
+    this.handleIndexResume();
+  },
+  methods: {
+    handleIndexResume: function (id) {
+      axios.get(`http://localhost:3000/students/${id}.json`).then((response) => {
+        console.log("students index", response);
+        this.students = response.data;
+      });
+    },
+  },
+};
+</script>
 
 <template>
   <main>
     <h1>Welcome to Vue!</h1>
+    <IndexResume />
+
+    <!-- <ShowResume /> -->
   </main>
 </template>
 
---
+++ src/components/IndexResume.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    students: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All Students</h1>
+    <div v-for="student in students" v-bind:key="student.id">
+      <h2>{{ student.first_name }}</h2>
+    </div>
+  </div>
+</template>
+
+<style></style>
