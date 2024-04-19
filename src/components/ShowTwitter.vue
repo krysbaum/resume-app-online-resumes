e6c90b25176522edb644102ea74d5cf5a2be2576
Krys <krysbaum@gmail.com>
2024-04-18 17:38:37 -0700

    Update twitter embed and showResume - in progress

--- src/components/ShowTwitter.vue
+++ src/components/ShowTwitter.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { useScriptTag } from "@vueuse/core";
+
+useScriptTag("https://platform.twitter.com/widgets.js");
+
+const props = defineProps({
+  students: Object,
+});
+
+// export default {
+//   props: {
+//     students: Object,
+//   },
+// };
+</script>
+
 <template>
-  <p>twitter: {{ student.twitter_handle }}</p>
+  <div>
+    <!-- <h1>{{ student.twitter_handle }}</h1> -->
+    <a class="twitter-timeline" href="https://twitter.com/concernedape?ref_src=twsrc%5Etfw"></a>
+  </div>
 </template>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import About from "./components/About.vue";
 import Content from "./components/Content.vue";
+import ShowResume from "./components/ShowResume.vue";
 
 const routes = [
   { path: "/", component: Content },
   { path: "/about", component: About },
+  { path: "/student/:student.id", component: ShowResume },
 ];
 
 const router = createRouter({
